6df071dbb4ab2ec200e17e7682d64b2b1123bc7e
sivashanker <dreamsivashanker@gmail.com>
2020-08-05 21:41:03 +0530

    completed signin handle submit

--- src/App.js
+++ src/App.js
@@ -30,12 +30,9 @@ class App extends React.Component {
                         ...snapShot.data()
                 } 
               });
-              console.log(this.state);
             })
         }
       this.setState({ currentUser: userAuth });
-
-      console.log(userAuth);
     });
   }
 
